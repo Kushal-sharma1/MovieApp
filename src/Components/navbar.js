e7b4b8144a18e7a39c90d156be2a83e8001769d4
Kushal-sharma1 <sharma.k.kushal@gmail.com>
2022-07-07 17:50:34 +0530

    :tada:start movieapp

---
+++ src/Components/navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class navbar extends Component {
+  render() {
+    return (
+      <>
+      <div style ={{display :'flex',justifyContent:'center', backgroundColor:'rgb(255, 80, 80)'}}>
+        <h1 style={{margin:'10px'}}>Movie App</h1>
+        <h1 style={{margin:'10px'}}>Favourite</h1>
+      </div>
+      
+      </>
+    )
+  }
+}
