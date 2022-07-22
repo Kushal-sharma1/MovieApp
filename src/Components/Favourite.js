ca02b7836259206232ce5b06e9df409113e3de8a
Kushal-sharma1 <sharma.k.kushal@gmail.com>
2022-07-23 01:37:40 +0530

    :sparkles: searching bar

--- src/Components/Favourite.js
+++ src/Components/Favourite.js
@@ -8,10 +8,27 @@ constructor(){
   this.state ={
     movies :[],
     genere:[],
-    currGen:"All genere"
+    currGen:"All genere",
+    currText:""
   }
 }
+handleText =(e)=>{
 
+  this.setState(
+    {
+      currText: e.target.value
+    }
+  )
+}
+handleclick=(ele)=>{
+  this.setState(
+   {
+     currGen: 
+     ele
+   }
+  )
+ 
+  }
  async componentDidMount(){
   let genereId={28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
     27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'}
@@ -38,18 +55,40 @@ results.map((movieobj)=>{
   )
  
 }
- handleclick=(ele)=>{
- this.setState(
-  {
-    currGen: 
-    ele
-  }
- )
 
- }
   render() {
     let genreId={28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
-    27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'}
+    27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'};
+    let FavouriteFilter =[];
+
+
+    if(this.state.currGen !="All genere"){
+
+      FavouriteFilter = this.state.movies.filter((obj)=>
+         genreId[obj. genre_ids[0]]==this.state.currGen
+      )
+      //on the basis of search text
+      if(this.state.currText!=''){
+       FavouriteFilter = FavouriteFilter.filter((obj)=>{
+       
+        let moviename = obj.original_title.toLowerCase();
+         return moviename.includes(this.state.currText);
+
+       })
+      }
+    }else{
+      FavouriteFilter =this.state.movies;
+      //on the basis of search text
+      if(this.state.currText!=''){
+        FavouriteFilter = FavouriteFilter.filter((obj)=>{
+        
+         let moviename = obj.original_title.toLowerCase();
+          return moviename.includes(this.state.currText);
+ 
+        })
+       }
+    }
+   
     return (
       <>
         <div class="container">
@@ -72,7 +111,7 @@ results.map((movieobj)=>{
             </div>
             <div class="col">
                 <div class="row"> 
-                     <input type={'text'} placeholder='Search' className='col-8'></input>
+                     <input type={'text'} placeholder='Search' className='col-8' value={this.state.currText} onChange={this.handleText}></input>
                      <input type ={'number'} placeholder='5' className='col-4'></input>
                 </div>
                         <table class="table">
@@ -86,15 +125,28 @@ results.map((movieobj)=>{
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                           
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td>Otto</td>
-                                <td><button type="button" class="btn btn-outline-danger">Delete</button></td>
+                      {
+                            FavouriteFilter.map((movieobj)=>(
+                              <tr>
+                                                                
+                              <td>
+                              <img src={`https://image.tmdb.org/t/p/original${movieobj.backdrop_path}`} style ={{ width:'8rem'}}alt="..."/>
+                              {movieobj.title}
+                              </td>
+                              <td>{genreId[movieobj.genre_ids[0]]}</td>
+                              <td>{movieobj.popularity}</td>
+                              <td>{movieobj.vote_average}</td>
+                              <td><button type="button" class="btn btn-outline-danger">Delete</button></td>
 
-                            </tr>
+                              </tr>
+
+
+                            ))   
+     
+
+
+                      }
+
                       
                         </tbody>
                         </table>
